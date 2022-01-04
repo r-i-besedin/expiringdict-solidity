1d8ba2f82b5303240e18d14d9437a267db743bd4
roman <besedin-r-i@yandex.ru>
2022-01-04 16:20:51 -0500

    Update HardHat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,14 +2,24 @@
  * @type import('hardhat/config').HardhatUserConfig
  */
 require('@nomiclabs/hardhat-ethers');
-const { alchemyApiKey, mnemonic } = require('./secrets.json');
 
-module.exports = {
-  solidity: "0.7.3",
-  networks: {
-    rinkeby: {
-      url: `https://eth-rinkeby.alchemyapi.io/v2/${alchemyApiKey}`,
-      accounts: {mnemonic: mnemonic}
+try {
+  const { alchemyApiKey, mnemonic } = require('./secrets.json');
+  
+  module.exports = {
+    solidity: "0.7.3",
+    networks: {
+      rinkeby: {
+        url: `https://eth-rinkeby.alchemyapi.io/v2/${alchemyApiKey}`,
+        accounts: {mnemonic: mnemonic}
+      }
     }
+  };
+} catch (e) {
+  if (e.code !== 'MODULE_NOT_FOUND') {
+    throw e;
   }
-};
+  module.exports = {
+    solidity: "0.7.3",
+  };
+}
